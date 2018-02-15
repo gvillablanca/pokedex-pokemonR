9787d64a5ec6032291baf304d02f5eb4b2590c21
adolivaresl <domioli@gmail.com>
2018-02-15 10:51:34 -0300

    muestra pokemones falta hacer funcional el imput de busqueda

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+const form = document.getElementById('search-form');
+	
+const param = $('.search-keyword').val(); 
+const pokeName = JSON.parse(param);
+//const param = document.getElementById('search-keyword').val;
 //asignamos la url a la variable getPokemon
 $(document).ready(() => {
   getPokemon('https://pokeapi.co/api/v2/pokemon');
+  pokeSubmit('http://pokeapi.co/api/v1/pokemon/' + pokeName);
 });	
 
 /*
@@ -34,14 +40,49 @@ const getPokemon = url => {
 const showPokemon = arr => {
 	let showStyle = '';
 	for(let i = 0; i < Math.random(arr.length / 20); i++){
-		showStyle += "<div class='col-sm-5 col-md-2'>";
+		showStyle += "<div class='col-sm-8 col-md-8'>";
 		for (let j = 0; j < 20; j++){
 			showStyle += '<div class="thumbnail pokemonBox">';
 			showStyle += `<img src="https://img.pokemondb.net/sprites/ruby-sapphire/normal/${arr[20*i + j].name}.png" class="responsive-img">`;
-			showStyle += `<h4>${arr[20*i + j].name}`;
+			showStyle += `<h4>${arr[20*i + j].name}`;	
 			showStyle += '</h4></div>'
 		}
 		showStyle += "</div>"
 	}
 	$("#contPokemon").append(showStyle);
+}
+/*---------------------------------------------- */
+
+
+const pokeSubmit= url => {
+	$.ajax({
+		url,
+		data: {'number': 'data.national_id',
+		'nombre': 'data.name'
+	},
+	}).done(responseAdd => {
+		showDataPokemon(responseAdd.results);
+	})
+	.fail(() => {
+		alert('La busqueda no dio resultado');
+	})
+	.always(() => {
+		console.log('tu pokemon es' + pokeName);
+	});	
+};
+
+form.addEventListener('submit', function(e) {
+	e.preventDefault();
+	
+})
+const showDataPokemon = data => {
+	let showMyPokemon = '';
+	showMyPokemon += "<div class='col-sm-6 col-md-6 col-sm-offset-3 col-md-offset-3'>";
+	showMyPokemon += '<div class= "thumbnail myPokemonBox">';
+	showMyPokemon += `<img src="https://img.pokemondb.net/sprites/ruby-sapphire/normal/${pokeName}.png" class="responsive-img">`;
+	showMyPokemon += `<h3>${param}`;
+	showMyPokemon += '</h3></div>'
+	showMyPokemon += "</div>"
+
+	$("#myPokemon").append(showMyPokemon);
 }

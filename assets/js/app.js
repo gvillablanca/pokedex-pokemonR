a684698744aad4a2885e7802567f49d2310c56f8
adolivaresl <domioli@gmail.com>
2018-02-16 02:44:43 -0300

    agregaron algunos estilos

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,7 +20,7 @@ $(document).ready(() => {
 })
 
 function showPokeInfo(info) {
-    $('#showData').append(`<div class="col-md-6 col-sm-6"><img src="https://pokeapi.co/media/img/${info.id}.png" class="pokemonVisual"><h5>Nombre:${info.name}</h5><p>Id:${info.id}</p><p>Order: ${info.order}</p></div>`);    
+    $('#showData').append(`<div class="col-md-2 col-sm-2 boxResults"><img src="https://pokeapi.co/media/img/${info.id}.png" class="pokemonVisual"><h5>Nombre: ${info.name}</h5><p>Id: ${info.id}</p><p>Orden: ${info.order}</p></div>`);    
 };
 
 const getPokemon = url => {
@@ -41,12 +41,8 @@ const getPokemon = url => {
 	});	
 };
 
-/*
-* En showPokemon creamos el código para que los pokemones se muestren en la página
-* 
- */
 const showPokemon = ()=> {
-	for(let i = 1; i<= 100 ; i++){
+	for(let i = 1; i<= 108 ; i++){
 		$('#contPokemon').append(`<img id="${i}" src="http://pokeapi.co/media/img/${i}.png">`);
 	}
 }
